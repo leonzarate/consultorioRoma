89a2229ed2cc8eb175cfee2c1b9a0cad9ece666a
Mauricio Leon Zarate <leonzarate@gmail.com>
2022-06-10 18:52:15 -0300

    Fresh installation

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/{nombre?}', function ($nombre = "Invitado") {
+    //return view('welcome');
+    return "Hola ". $nombre;
 });
